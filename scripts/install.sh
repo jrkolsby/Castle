22e87518c0117a4fa618c5a8267a5e1e69ea518d
James Kolsby <jrkolsby@mac.com>
2018-07-24 13:10:43 -0400

    fix: git completion

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,4 +5,4 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s ~/.vim/config/.vimrc ~/.vimrc
 ln -s ~/.vim/config/.tmux.conf ~/.tmux.conf
 ln -s ~/.vim/config/.bash_profile ~/.bash_profile
-ln -s ~/.vim/scripts/git_completion.sh ~/.git_completion.sh
+ln -s ~/.vim/scripts/git-completion.sh ~/.git-completion.sh
