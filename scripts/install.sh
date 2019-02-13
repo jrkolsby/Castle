897c8a4bb2ea476213a10d75d2e33ff3cc73d493
James <james@co.jmkl>
2019-02-13 12:34:31 -0500

    fix: CentOS install compatibility

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [[ -f ~/.vim/bundle/Vundle.vim ]]; then
+        git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 
-ln -s ~/.vim/config/.vimrc ~/.vimrc
-ln -s ~/.vim/config/.tmux.conf ~/.tmux.conf
-ln -s ~/.vim/config/.bash_profile ~/.bash_profile
-ln -s ~/.vim/scripts/git-completion.sh ~/.git-completion.sh
+if [[ -f ~/.vimrc ]]; then 
+        echo "backup .vimrc.bak"
+        mv ~/.vimrc ~/.vimrc.bak 
+fi
+if [[ -f ~/.bashrc ]]; then 
+        echo "backup .bashrc.bak"
+        mv ~/.bashrc ~/.bashrc.bak
+fi
+if [[ -f ~/.tmux.conf ]]; then 
+        echo "backup .tmux.conf.bak"
+        mv ~/.tmux.conf ~/.tmux.conf.bak 
+fi
+if [[ -f ~/.bash_profile ]]; then 
+        echo "backup .bash_profile.bak"
+        mv ~/.bash_profile ~/.bash_profile.bak 
+fi
+
+ln -sf ~/.vim/config/.vimrc ~/.vimrc
+ln -sf ~/.vim/config/.tmux.conf ~/.tmux.conf
+ln -sf ~/.vim/config/.bash_profile ~/.bashrc
+ln -sf ~/.vim/config/.bash_profile ~/.bash_profile
+ln -sf ~/.vim/scripts/git-completion.sh ~/.git-completion.sh
